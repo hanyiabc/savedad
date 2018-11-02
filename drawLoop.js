152514f9fe4291c64227dc1f655f50ae62af2b0b
guancongyi <congyiguan@gmail.com>
2018-11-02 19:00:09 -0400

    spider code

--- drawLoop.js
+++ drawLoop.js
@@ -5,9 +5,46 @@ var sketchProc = function (processingInstance) {
         size(1280, 720);
         frameRate(60);
         angleMode = "radiant";
+        var state = 0;
+
         var mainChar = [];
-        mainChar.push(loadImage("assets/main_front.png"))
-        mainChar.push(loadImage("assets/main_back.png"))
+        mainChar.push(loadImage("assets/main_front.png"));
+        mainChar.push(loadImage("assets/main_back.png"));
+
+        
+        
+        var spiderObj = function(x,y){
+            this.position = new PVector(x, y);
+            this.size = 60;
+            this.state = 0;
+            this.spider = [];
+            this.currFrame = frameCount;
+            this.spider.push(loadImage("assets/spider1.png"))
+            this.spider.push(loadImage("assets/spider2.png"))
+            this.spider.push(loadImage("assets/spider3.png"));
+        }
+        var spiders = new spiderObj(300,300);
+        spiderObj.prototype.draw = function(){
+            switch(this.state){
+                case 0:
+                image(this.spider[0], this.position.x, this.position.y, 60, 60);
+                break;
+                case 1:
+                image(this.spider[1], this.position.x, this.position.y, 60, 60);
+                break;
+                case 2:
+                image(this.spider[2], this.position.x, this.position.y, 60, 60);
+                break;
+            }
+            this.state++;
+            if (this.state> 2) {
+                this.state = 0;
+            }
+            if (this.currFrame) {
+                this.currFrame = frameCount;
+                
+            }
+        }
 
         var MainChar = function (x, y) {
             this.position = new PVector(x, y);
@@ -16,9 +53,13 @@ var sketchProc = function (processingInstance) {
         };
 
         var draw = function () {
+            if (state === 1)
+            {
+                image(mainChar[0], 500, 500, 60, 60);
+                image(mainChar[1], 300, 400, 60, 60);
+                spiders.draw();
+            }
             
-            image(mainChar[0], 500, 500, 60, 60);
-            image(mainChar[1], 300, 400, 60, 60);
 
         };
 
